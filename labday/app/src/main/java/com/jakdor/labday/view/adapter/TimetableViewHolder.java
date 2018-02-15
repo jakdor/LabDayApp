02521ac2f0d4f5687e2f6776643d5df0ffac9ac6
Jakub Dorda <jak_dor@wp.pl>
2018-02-15 02:57:53 +0100

    modified timetable_item.xml, time formatting in TimetableViewHolder

--- labday/app/src/main/java/com/jakdor/labday/view/adapter/TimetableViewHolder.java
+++ labday/app/src/main/java/com/jakdor/labday/view/adapter/TimetableViewHolder.java
@@ -1,10 +1,16 @@
 package com.jakdor.labday.view.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.jakdor.labday.common.model.Event;
 import com.jakdor.labday.common.model.Timetable;
 import com.jakdor.labday.databinding.TimetableItemBinding;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 /**
  * ViewHolder for timetable_item.xml
@@ -19,6 +25,14 @@ public class TimetableViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void bind(Timetable timetable, Event event){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm", Locale.GERMAN);
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        Date start = new Date(timetable.getTimeStart()*1000);
+        Date end = new Date(timetable.getTimeEnd()*1000);
+
+        binding.setTimeStart(simpleDateFormat.format(start));
+        binding.setTimeEnd(simpleDateFormat.format(end));
+
         binding.setTimetable(timetable);
         binding.setEvent(event);
         binding.executePendingBindings();
