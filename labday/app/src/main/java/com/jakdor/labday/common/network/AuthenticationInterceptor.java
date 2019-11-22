119835a285877b019826e1926c44ab4a320514a9
Jakub Dorda <jak_dor@wp.pl>
2019-11-22 19:30:43 +0100

    minor networking refactor, adjust for new REST API

--- labday/app/src/main/java/com/jakdor/labday/common/network/AuthenticationInterceptor.java
+++ labday/app/src/main/java/com/jakdor/labday/common/network/AuthenticationInterceptor.java
@@ -15,7 +15,7 @@ public class AuthenticationInterceptor implements Interceptor {
         Request original = chain.request();
 
         Request.Builder builder = original.newBuilder()
-                .header("Authorization", "token " + authToken);
+                .header("Authorization", "Bearer " + authToken);
 
         Request request = builder.build();
         return chain.proceed(request);
